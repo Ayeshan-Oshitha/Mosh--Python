df8cdc9a5b6570b89ff404ceb11bafba582e0591
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-05-15 11:58:48 +0530

    Completed the Python Package Index Section

---
+++ 010-Python-Package-Index/002-Docstring/app.py
@@ -0,0 +1,19 @@
+"""
+This module provides a function to convert a PDF file to text.
+"""
+
+
+def convert():
+    """
+    Converts a given PDF file to text.
+
+    Parameters:
+        path (str): The path to the PDF file.
+
+    Returns:
+        str: The content of the PDF file as plain text.
+    """
+    print("Hello, World!")
+
+
+convert()
